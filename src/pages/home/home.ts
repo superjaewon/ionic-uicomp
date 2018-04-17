177e2fe06c0c2443e7d84b020accc1020dadd8a0
superjaewon <superjaewon@naver.com>
2018-04-17 19:48:52 +0900

    userName 변수추가

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items = [
+    {"id":1, name:"첫번째 item"},
+    {"id":2, name:"두번째 item"},
+    {"id":3, name:"세번째 item"},
+  ]
+
+  itemselected(item){
+    alert(item.id);
+  }
+
+  clickButton(event){
+
+  }
   userName:any;
 
 
