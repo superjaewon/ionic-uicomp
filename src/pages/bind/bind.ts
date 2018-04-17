c7eb3315c00b9ba30997a76289c8c4cad8550e61
superjaewon <superjaewon@naver.com>
2018-04-17 21:05:48 +0900

    bind페이지 추가

---
+++ src/pages/bind/bind.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the BindPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({name:'bindPage',})
+@Component({
+  selector: 'page-bind',
+  templateUrl: 'bind.html',
+})
+export class BindPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  //life cycle method 화면이 load되고 난 후에 바로 호출되는 메소드
+  ionViewDidLoad() {
+    //alert('ionViewDidLoad BindPage '+ this.navParams.get("name")); //파라미터로 받은 값 가져오기
+    //this.test(this.navParams.get("name"));
+    this.test(this.navParams.get("name"));
+  }
+
+  goBack(){
+    this.navCtrl.pop();
+  }
+
+  test(name){
+    alert(name);
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,14 +16,25 @@ export class HomePage {
     alert(item.id);
   }
 
-  clickButton(event){
+  myButton(event){
+    //alert(event.target.textContent)
+    //console.log(event.target.textContent);
+    if(typeof this.userName !== "undefined"){ //userName이 정의되어 있을경우
+      //this.navCtrl.push('bindPage');
+      this.navCtrl.push('bindPage',{name:this.userName}); //page:이동 할 페이지명, params:넘길 값
+    }else{
+      alert('userName 입력!!!');
+    }
 
   }
+
+  disabledSwitch = false;
+
   userName:any;
 
 
   constructor(public navCtrl: NavController) {
-    this.userName = "아이오닉";
+    //this.userName = "아이오닉";
   }
 
 }
